311e800750a3d5d1a123c08f0937dedba08f4f01
Franklin <43157406+HyDDRo@users.noreply.github.com>
2019-03-28 20:30:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 alert("this script works!");
+window.addEventListner("load", ()=> {
+  let long;
+  let lat;
+     
+   if(navigator.geolocation){
+        navigation.geolocation.getCurrentPosition
+        (position => {
+          console.log(position);
+         
+        });
+   }else{
+     h1.textContent = "hey this dosnt work"
+     
+     
+   });
